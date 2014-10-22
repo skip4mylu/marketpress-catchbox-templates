a100817de12b1734c6b238e07e456aed9a9da5a8
skip4mylu <andy@andyeulau.com>
2014-10-22 08:49:14 -0400

    Create content-product-list.php

---
+++ content-product-list.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template used for displaying page content in page.php
+ * modified for use in marketpress to display content-product-list in mp_product-list.php
+ * @package Catch Themes
+ * @subpackage Catch_Box
+ * @since Catch Box 1.0
+ */
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="entry-header">
+	<h2 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'catchbox' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
+	</header><!-- .entry-header -->
+
+	<div class="entry-content">
+
+		<?php
+			mp_product_image( true, 'list' ); // 'list' because we want to link to product.
+			the_excerpt();
+			mp_product_meta( true, 'single' ); // 'single' because we want to show option drop-down  and buy button.
+		?>
+
+	</div><!-- .entry-content -->
+	<footer class="entry-meta">
+		<?php edit_post_link( __( 'Edit', 'catchbox' ), '<span class="edit-link">', '</span>' ); ?>
+	</footer><!-- .entry-meta -->
+</article><!-- #post-<?php the_ID(); ?> -->
